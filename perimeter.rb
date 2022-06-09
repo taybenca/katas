f7f184b603349e31406aef4e11fedc396f42b8bf
Tayanne Bencardino <tayanne@Tayannes-MacBook-Air.local>
2022-06-09 19:44:10 +0100

    Problem solved

--- perimeter.rb
+++ perimeter.rb
@@ -6,4 +6,29 @@
 # when there are n + 1 squares disposed in the same manner as in the drawing:
 
 # Hint:
-# See Fibonacci sequence
+# See Fibonacci 
+
+# perimeter(5)  should return 80
+# perimeter(7)  should return 216
+
+# Find fibonacci sequence and sum them
+def fib(n)
+  f1 = 1
+  f2 = 1
+  @fib_seq = [1, 1]
+  while @fib_seq.length < n+1 do
+    next_number = f1 + f2
+    f2 = f1
+    f1 = next_number
+    @fib_seq << next_number
+  end
+  return @fib_seq
+end
+
+# Find perimeter
+def perimeter(fib_seq)
+    return fib_seq.sum * 4
+end
+
+fib(7)
+puts perimeter(@fib_seq)
